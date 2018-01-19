63c493164006ca6bc6bd0a4cf0d4815294993842
dcluet <david.cluet@ens-lyon.fr>
2018-01-19 13:03:46 +0100

    Installation macro and Refresh for development ready.

---
+++ src/Refresh.java
@@ -0,0 +1,34 @@
+macro "Refresh" {
+
+    //Prepare key paths
+    Path = File.separator()+"home.users";
+    Path += File.separator()+"dcluet";
+    Path += File.separator()+"Programmes";
+    Path += File.separator()+"2017_Auboeuf_PLA";
+    Path += File.separator()+"src";
+
+    PathFolderInput = Path+File.separator+"Macro"+File.separator;
+    PathOutput = getDirectory("macros")+"PLA"+File.separator;
+
+    //Listing of the files to instal
+    Listing = newArray("Main.java");
+
+    //Create the installation folder if required
+    if(File.exists(PathOutput)==0){
+        File.makeDirectory(getDirectory("macros")+File.separator+"PLA");
+    }
+
+    //Update files or create them
+    for(i=0; i<lengthOf(Listing); i++){
+    	if(File.exists(PathFolderInput+Listing[i])==0){
+        	exit("" + PathFolderInput+Listing[i] + " file is missing");
+    	}else{
+			Transfer=File.copy(PathFolderInput+Listing[i], PathOutput+Listing[i]);
+		}
+	}
+
+    //Launch main macro
+    Path = PathOutput + "Main.java";
+    runMacro(Path);
+
+}
